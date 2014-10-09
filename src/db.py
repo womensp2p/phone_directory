77362f4ced0f323d3d88e4a7772c709999a6cf2a
Ben Congleton <ben@olark.com>
2014-10-08 17:12:29 -0700

    Added database

---
+++ src/db.py
@@ -0,0 +1,150 @@
+
+import datetime
+import re
+
+import sqlalchemy
+from sqlalchemy import Boolean, Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+#from phone_directory_actions import PhoneDirectoryActions
+
+
+Base = declarative_base()
+
+def bind_engine(engine):
+    Base.metadata.create_all(engine)
+    return sessionmaker(bind=engine, autoflush=True)
+
+# def __create_crm_object(SessionMaker, crm_type, crm_class, kwargs,
+#         site_id=None):
+#
+#     session = SessionMaker()
+#
+#     connection = crm_class(**kwargs)
+#     connection.site_id = site_id
+#     session.add(connection)
+#     session.commit()
+#
+#     crm_id = connection.crm_id
+#     session.close()
+#
+#     return crm_id
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column('user_id', Integer, primary_key=True)
+    phone = Column('crm_type', String(100))
+    name = Column('name', String(100))
+
+    updated_at = Column('updated_at', DateTime, onupdate=datetime.datetime.now, default=datetime.datetime.now, index=True)
+    created_at = Column('created_at', DateTime, default=datetime.datetime.now, index=True)
+
+class PhoneHistory(Base):
+    __tablename__ = 'phone_history'
+    id = Column('id', Integer, index=True, primary_key=True)
+    user_id = Column('user_id', Integer, index=True)
+    phone = Column('phone', String(100))
+    updated_at = Column('updated_at', DateTime, onupdate=datetime.datetime.now, default=datetime.datetime.now, index=True)
+    created_at = Column('created_at', DateTime, default=datetime.datetime.now, index=True)
+
+
+class PhoneDirectoryActions:
+    pass
+
+class SqlitePhoneDirectoryActions(PhoneDirectoryActions):
+    '''
+    Data stored in a sqliteDB.
+    '''
+
+    def __init__(self, database="sqlite:///test.db"):
+        engine = sqlalchemy.create_engine(
+            database # TODO: better path to database
+        )
+
+        self._SessionMaker = bind_engine(engine)
+
+    def _get_user_by_id(self, user_id):
+        session = self._SessionMaker()
+        res = session.query(User).filter(User.user_id == user_id).all()
+        session.close()
+
+        if len(res) != 1:
+            raise KeyError
+        else:
+            return res[0]
+
+    def user_exists(self, user_id):
+        '''
+        Return true if user_id exists, false if not
+        '''
+        user = self._get_user_by_id(user_id)
+        return (user != None) # truthy
+
+    def get_phone_number(self, user_id):
+        '''
+        Return phone number for given user_id. Throw KeyError if user doesn't exist.
+        '''
+        user = self._get_user_by_id(user_id)
+        return user.phone
+
+    def get_name(self, user_id):
+        '''
+        Return username for given user_id. Throw KeyError if user doesn't exist.
+        '''
+        user = self._get_user_by_id(user_id)
+        return user.name
+
+    def update_phone_number(self, user_id, new_number):
+        '''
+        If user_id exists, update its phone number and archive the old number.
+        If user_id does not exist, throw KeyError
+        If new phone number is not valid, throw
+        '''
+        user = self._get_user_by_id(user_id)
+        self._validate_phone_number(new_number)
+        old_number = user.phone
+        user.phone = new_number
+
+        session = self._SessionMaker()
+
+        history = PhoneHistory()
+        history.user_id = user.user_id
+        history.phone = old_number
+
+        session.add(user)
+        session.add(history)
+        session.commit()
+
+    def _add_a_user(self, user_id, phone, name):
+        session = self._SessionMaker()
+
+        user= User()
+        user.name = name
+        user.user_id = user_id
+        user.phone = phone
+
+        session.add(user)
+        session.commit()
+
+    def _validate_phone_number(self, number):
+        '''
+        Raise ValueError if phone number is not a ten digit numeric string
+        '''
+        if not (re.match(r'^\d+$', number) and len(number) == 10):
+            raise ValueError('Phone number {} is not a ten digit numeric string'.format(number))
+
+
+if __name__ == '__main__':
+    sq = SqlitePhoneDirectoryActions(database="sqlite:///:memory:")
+    sq._add_a_user(user_id=1122, phone='1'*10, name='ben')
+
+    # some quick tests
+    assert sq.user_exists(1122)
+    assert sq.get_name(1122) == 'ben'
+    assert sq.get_phone_number(1122) == '1'*10
+    sq.update_phone_number(user_id=1122, new_number='2'*10)
+    assert sq.get_phone_number(1122) == '2'*10
+
+
